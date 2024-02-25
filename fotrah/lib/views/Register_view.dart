d038fd7d38890409602ca040a172c3808fd1f20e
Apeto13 <abdulrahmanalsalman13@gmail.com>
2024-02-26 00:56:57 +0300

    Error handling for Register view

--- fotrah/lib/views/Register_view.dart
+++ fotrah/lib/views/Register_view.dart
@@ -5,6 +5,8 @@ import 'package:flutter/widgets.dart';
 import 'package:fotrah/constants/routes.dart';
 import 'package:fotrah/firebase_options.dart';
 import 'dart:developer' as devtools show log;
+import 'package:fotrah/utilities/show_error_dialog.dart';
+
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
 
@@ -97,20 +99,27 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final userCredential =
-                    await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                await FirebaseAuth.instance.createUserWithEmailAndPassword(
                   email: email,
                   password: password,
                 );
-                devtools.log(userCredential.toString());
+                final user = FirebaseAuth.instance.currentUser;
+                await user?.sendEmailVerification();
+                Navigator.of(context).pushNamed(VerifyEmailRoute);
               } on FirebaseAuthException catch (e) {
                 devtools.log(e.code);
-                if (e.code == 'weak-password')
-                  devtools.log("Weak password! ");
-                else if (e.code == "email-already-in-use")
-                  devtools.log("This email is already in use! ");
-                else if (e.code == "invalid-email")
-                  devtools.log("This email is invaild! ");
+                if (e.code == 'weak-password') {
+                  await showErrorDialog(
+                      context, "The password you are using is weak!");
+                } else if (e.code == "email-already-in-use") {
+                  await showErrorDialog(context, "Email already in use!");
+                } else if (e.code == "invalid-email") {
+                  await showErrorDialog(context, "email does not exist!");
+                } else {
+                  await showErrorDialog(context, e.code);
+                }
+              } catch (e) {
+                await showErrorDialog(context, "Error ${e.toString()}");
               }
             },
             child: const Text('Register'),
